a5238ebde8b2c94b714c29f4baffc7046ab9bb40
Jordan Garrison <jordan.andrew.garrison@gmail.com>
2019-07-24 00:19:13 -0500

    Updates from learning new org mode bits

--- init.el
+++ init.el
@@ -48,8 +48,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(blink-cursor-mode nil)
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
  '(cursor-type (quote bar))
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-safe-themes
+   (quote
+    ("57f95012730e3a03ebddb7f2925861ade87f53d5bbb255398357731a7b1ac0e0" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(org-babel-load-languages
    (quote
     ((python . t)
@@ -60,7 +72,30 @@
      (C . t))))
  '(package-selected-packages
    (quote
-    (ox-hugo evil-smartparens smartparens evil flycheck-gometalinter go-add-tags go-complete go-direx go-dlv go-eldoc go-errcheck go-fill-struct go-gen-test go-guru go-imports go-playground go-playground-cli go-scratch go-snippets go-stacktracer godoctor golint govc govet ob-go zenburn-theme zenburn org-ac neotree flymake-go go-autocomplete auto-complete-auctex exec-path-from-shell go go-mode ox-reveal yasnippet elpy jedi flycheck ace-window which-key use-package try tabbar-ruler org-bullets counsel color-theme avy auto-complete atom-one-dark-theme)))
+    (docker docker-tramp docker-compose-mode dockerfile-mode flycheck-mmark markdown-mode+ ox-hugo evil-smartparens smartparens evil flycheck-gometalinter go-add-tags go-complete go-direx go-dlv go-eldoc go-errcheck go-fill-struct go-gen-test go-guru go-imports go-playground go-playground-cli go-scratch go-snippets go-stacktracer godoctor golint govc govet ob-go zenburn-theme zenburn org-ac neotree flymake-go go-autocomplete auto-complete-auctex exec-path-from-shell go go-mode ox-reveal yasnippet elpy jedi flycheck ace-window which-key use-package try tabbar-ruler org-bullets counsel color-theme avy auto-complete atom-one-dark-theme)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
  '(visible-cursor t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
